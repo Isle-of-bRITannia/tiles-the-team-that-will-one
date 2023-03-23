ff886bc623f496faf2b1bc891fcc77cceea83b81
gph6823 <gph6823@rit.edu>
2023-03-23 11:52:31 -0400

    wizardtime

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import { Tile } from './tiles/api.js';
 
-import { fireGirl, uh } from './tiles/imageTiles/index.js';
+import { fireGirl, purpleWiz, uh, redWiz, fireBall } from './tiles/imageTiles/index.js';
 import { rasterize } from './tiles/observe/rasterize.js';
 import { renderRaster } from './tiles/observe/renderRaster.js';
 
@@ -30,10 +30,49 @@ const ctx = canvas?.getContext('2d');
 
 //const tile = Tile.swirl(Tile.above(Tile.swirl(Tile.above(Tile.pure('red'),fireGirl)),Tile.beside(uh, Tile.pure('blue'))));
 
-const tile = Tile.swirl(
-  Tile.above(
-    Tile.quad(Tile.pure('blue'), Tile.pure('yellow'), Tile.pure('red'), Tile.pure('green')),
-    uh
+const tile = Tile.above(
+  Tile.beside(
+    Tile.quad(
+      redWiz,
+      fireBall,
+      Tile.quad(
+        redWiz,
+        redWiz,
+        fireBall,
+        redWiz
+      ),
+      redWiz
+    ),
+    Tile.beside(
+      fireBall,
+      purpleWiz
+      )
+  )
+  ,
+  Tile.beside(
+    Tile.quad(
+      fireBall,
+      redWiz,
+      fireBall,
+      redWiz
+    ),
+    Tile.beside(
+      Tile.quad(
+        redWiz,
+        Tile.quad(
+          redWiz,
+          redWiz,
+          purpleWiz,
+          purpleWiz
+        ),
+        redWiz,
+        Tile.above(
+          purpleWiz,
+          fireBall
+        )
+      ),
+      purpleWiz
+    )
   )
 ); 
 
